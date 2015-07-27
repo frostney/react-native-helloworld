2b79042a729ecc38734fbbd07fa07683525b473a
Johannes Stein <johannesstein@freeze-dev.com>
2015-07-27 22:12:31 +0200

    Style update

--- index.ios.js
+++ index.ios.js
@@ -7,21 +7,30 @@ class HelloThere extends React.Component {
   }
   render() {
     return (
-      <TouchableOpacity onPress={this.clickMe.bind(this)}>
-        <View style={styles.box}>
-          <Text>Hello {this.props.name}. Please click me.</Text>
-        </View>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.clickMe.bind(this)}>
+          <View style={styles.box}>
+            <Text>Hello {this.props.name}. Please click me.</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
 var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   box: {
-    backgroundColor: 'red',
+    borderColor: 'red',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    padding: 10,
     width: 100,
     height: 100,
-    marginTop: 100
   }
 });
 
